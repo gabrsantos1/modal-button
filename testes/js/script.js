0b50348a3a1d3f87a2f7601c1fe18454f59fcc67
gabrsantos1 <165441225+gabrsantos1@users.noreply.github.com>
2024-04-15 12:50:26 -0300

    Add files via upload

---
+++ testes/js/script.js
@@ -0,0 +1,13 @@
+const openModalButton = document.querySelector("#abriModal");
+const closeModalButton = document.querySelector("#fecharModal");
+const modal = document.querySelector("#modal");
+const fade = document.querySelector("#fade");
+
+const toggleModal = () => {
+  modal.classList.toggle("hide");
+  fade.classList.toggle("hide");
+};
+
+[openModalButton, closeModalButton, fade].forEach((el) => {
+  el.addEventListener("click", () => toggleModal());
+});
